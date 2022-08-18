764f6f22635c56068933c6b5b1374cd42c8dbcb8
allanstar-byte <allanmutisya263@gmail.com>
2022-08-18 11:18:48 +0300

    settter/getter

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,24 @@
-class Dog
+require 'pry'
+class Person
+    attr_accessor :name, :breed
+    # attr_reader :name, :breed
 
-end
+    # setter method
+    def name=(name, breed = "sapiens")
+      @name = name
+      @breed = breed
+    end
+  
+    # getter method
+    # def name
+    #   @name
+    #   @breed
+    # end
+  
+  end
+
+doggie = Person.new
+doggie.name=("Bobi")
+doggie.breed = "Breed"
+binding.pry
+
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,22 @@
 class Person
-
+    attr_accessor :name, :job
+    # setter method
+    def name=(name, job = "doctor")
+      @name = name
+      @job = job
+    end
 end
+
+kanye = Person.new
+kanye.name=("Kanye")
+
+# class Person
+#     attr_accessor :name, :job
+#     def name = (name, job = "Doctor")
+#         @name = name
+#         @job = job
+#     end
+# end
+
+# usrs = Person.new
+# usrs.name = ("ll")
